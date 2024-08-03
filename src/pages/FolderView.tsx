12aebdbe5ea27fe7d82a9e061258c6cae1dd1df8
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-08-03 12:32:25 +0000

    Refactor localStorage key for staffId to userId

--- src/pages/FolderView.tsx
+++ src/pages/FolderView.tsx
@@ -1,142 +1,143 @@
-import FileUpload from "@/components/shared/fileUpload-button";
-import FolderCard from "@/components/shared/folder-card";
-import GridComponent from "@/components/shared/grid";
-import ListComponent from "@/components/shared/list";
-import FolderButton from "@/components/shared/newFolder-button";
-import { FileIcon, Music4Icon } from "lucide-react";
+// import FileUpload from "@/components/shared/fileUpload-button";
+// import FolderCard from "@/components/shared/folder-card";
+// import GridComponent from "@/components/shared/grid";
+// import ListComponent from "@/components/shared/list";
+// import FolderButton from "@/components/shared/newFolder-button";
+// import { FileIcon, Music4Icon } from "lucide-react";
 
 const FolderView = () => {
-  const folderList = [1, 2, 3, 4];
+  // const folderList = [1, 2, 3, 4];
   return (
-    <div className="p-4 w-full h-full">
-      <div className="w-full relative">
-        <div className="flex gap-4 items-center right-0 absolute">
-          <ListComponent />
-          <div className="hidden lg:block">
-            <GridComponent />
-          </div>
-          <FileUpload />
-          <FolderButton />
-        </div>
-      </div>
-      <div className="mt-10">
-        <p className="font-semibold">Folder 1</p>
-      </div>
-      <div className="flex flex-col gap-2 lg:grid lg:grid-cols-4 lg:gap-4 my-4">
-        {folderList.map((item) => (
-          <FolderCard key={item} />
-        ))}
-      </div>
-      <div className="grid grid-cols-2 lg:grid lg:grid-cols-4 gap-4 items-center">
-        {fileData.map((item) => (
-          <div className="flex flex-col">
-            <div className="w-40 h-40 bg-gray-300 rounded-sm">
-              {item.type === "png" || item.type === "jpg" ? (
-                <img
-                  src={item.img.toString()}
-                  alt={item.name}
-                  className="rounded-sm object-cover w-full h-full"
-                />
-              ) : (
-                <i className="flex justify-center mt-16">{item.img}</i>
-              )}
-            </div>
-            <div className="mt-2">
-              <p className="text-sm font-semibold">{`${item.name}.${item.type}`}</p>
-              <p className="text-xs font-medium text-gray-400">{item.size}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    // <div className="p-4 w-full h-full">
+    //   <div className="w-full relative">
+    //     <div className="flex gap-4 items-center right-0 absolute">
+    //       <ListComponent />
+    //       <div className="hidden lg:block">
+    //         <GridComponent />
+    //       </div>
+    //       <FileUpload />
+    //       <FolderButton />
+    //     </div>
+    //   </div>
+    //   <div className="mt-10">
+    //     <p className="font-semibold">Folder 1</p>
+    //   </div>
+    //   <div className="flex flex-col gap-2 lg:grid lg:grid-cols-4 lg:gap-4 my-4">
+    //     {folderList.map((item) => (
+    //       <FolderCard key={item} />
+    //     ))}
+    //   </div>
+    //   <div className="grid grid-cols-2 lg:grid lg:grid-cols-4 gap-4 items-center">
+    //     {fileData.map((item) => (
+    //       <div className="flex flex-col">
+    //         <div className="w-40 h-40 bg-gray-300 rounded-sm">
+    //           {item.type === "png" || item.type === "jpg" ? (
+    //             <img
+    //               src={item.img.toString()}
+    //               alt={item.name}
+    //               className="rounded-sm object-cover w-full h-full"
+    //             />
+    //           ) : (
+    //             <i className="flex justify-center mt-16">{item.img}</i>
+    //           )}
+    //         </div>
+    //         <div className="mt-2">
+    //           <p className="text-sm font-semibold">{`${item.name}.${item.type}`}</p>
+    //           <p className="text-xs font-medium text-gray-400">{item.size}</p>
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
+    <div className="">Folder View</div>
   );
 };
 
 export default FolderView;
 
-const fileData = [
-  {
-    id: 1,
-    name: "file1",
-    size: "2.5mb",
-    type: "png",
-    img: "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 2,
-    name: "file2",
-    size: "1.5mb",
-    type: "mp3",
-    img: <Music4Icon />,
-  },
-  {
-    id: 3,
-    name: "file3",
-    size: "3.5mb",
-    type: "pdf",
-    img: <FileIcon />,
-  },
-  {
-    id: 4,
-    name: "file4",
-    size: "2.5mb",
-    type: "doc",
-    img: <FileIcon />,
-  },
-  {
-    id: 5,
-    name: "file5",
-    size: "2.5mb",
-    type: "jpg",
-    img: "https://images.pexels.com/photos/1005417/pexels-photo-1005417.jpeg?auto=compress&cs=tinysrgb&w=600",
-  },
-  {
-    id: 6,
-    name: "file6",
-    size: "2.5mb",
-    type: "mp3",
-    img: <Music4Icon />,
-  },
-  {
-    id: 7,
-    name: "file7",
-    size: "2.5mb",
-    type: "pdf",
-    img: <FileIcon />,
-  },
-  {
-    id: 8,
-    name: "file8",
-    size: "2.5mb",
-    type: "png",
-    img: "https://images.pexels.com/photos/289586/pexels-photo-289586.jpeg?auto=compress&cs=tinysrgb&w=600",
-  },
-  {
-    id: 9,
-    name: "file9",
-    size: "2.5mb",
-    type: "mp3",
-    img: <Music4Icon />,
-  },
-  {
-    id: 10,
-    name: "file10",
-    size: "2.5mb",
-    type: "doc",
-    img: <FileIcon />,
-  },
-  {
-    id: 11,
-    name: "file11",
-    size: "2.5mb",
-    type: "pdf",
-    img: <FileIcon />,
-  },
-  {
-    id: 12,
-    name: "file12",
-    size: "2.5mb",
-    type: "pdf",
-    img: <FileIcon />,
-  },
-];
+// const fileData = [
+//   {
+//     id: 1,
+//     name: "file1",
+//     size: "2.5mb",
+//     type: "png",
+//     img: "https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//   },
+//   {
+//     id: 2,
+//     name: "file2",
+//     size: "1.5mb",
+//     type: "mp3",
+//     img: <Music4Icon />,
+//   },
+//   {
+//     id: 3,
+//     name: "file3",
+//     size: "3.5mb",
+//     type: "pdf",
+//     img: <FileIcon />,
+//   },
+//   {
+//     id: 4,
+//     name: "file4",
+//     size: "2.5mb",
+//     type: "doc",
+//     img: <FileIcon />,
+//   },
+//   {
+//     id: 5,
+//     name: "file5",
+//     size: "2.5mb",
+//     type: "jpg",
+//     img: "https://images.pexels.com/photos/1005417/pexels-photo-1005417.jpeg?auto=compress&cs=tinysrgb&w=600",
+//   },
+//   {
+//     id: 6,
+//     name: "file6",
+//     size: "2.5mb",
+//     type: "mp3",
+//     img: <Music4Icon />,
+//   },
+//   {
+//     id: 7,
+//     name: "file7",
+//     size: "2.5mb",
+//     type: "pdf",
+//     img: <FileIcon />,
+//   },
+//   {
+//     id: 8,
+//     name: "file8",
+//     size: "2.5mb",
+//     type: "png",
+//     img: "https://images.pexels.com/photos/289586/pexels-photo-289586.jpeg?auto=compress&cs=tinysrgb&w=600",
+//   },
+//   {
+//     id: 9,
+//     name: "file9",
+//     size: "2.5mb",
+//     type: "mp3",
+//     img: <Music4Icon />,
+//   },
+//   {
+//     id: 10,
+//     name: "file10",
+//     size: "2.5mb",
+//     type: "doc",
+//     img: <FileIcon />,
+//   },
+//   {
+//     id: 11,
+//     name: "file11",
+//     size: "2.5mb",
+//     type: "pdf",
+//     img: <FileIcon />,
+//   },
+//   {
+//     id: 12,
+//     name: "file12",
+//     size: "2.5mb",
+//     type: "pdf",
+//     img: <FileIcon />,
+//   },
+// ];
